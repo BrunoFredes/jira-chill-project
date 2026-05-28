e5754fd9e3288828aa922d6c4ba961b596c0d4ef
Bruno Fredes <123222626+BrunoFredes@users.noreply.github.com>
2026-05-28 11:18:06 -0300

    fix postgres ssl connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.connect() 
-    .then(() => console.log('Connected to the database successfully!'))
-    .catch(err => console.error('Database connection error:', err));
+pool.connect()
+  .then(() => console.log('Connected to the database successfully!'))
+  .catch(err => console.error('Database connection error:', err));
 
-module.exports = pool;    
+module.exports = pool;
