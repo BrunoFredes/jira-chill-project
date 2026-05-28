3d9907dcdc093af2c3469eeca7141f3182c08f70
Bruno Fredes <123222626+BrunoFredes@users.noreply.github.com>
2026-05-28 14:55:52 -0300

    database para render

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,10 @@
-const { Pool } = require('pg');
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
-  ssl: false
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
 });
-
-module.exports = pool;
